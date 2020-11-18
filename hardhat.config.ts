38ebc13830e8cce1146432fd830103a4cecf809b
leekt216 <leekt216@gmail.com>
2020-11-18 22:04:23 +0900

    cleaned up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
-//import "solidity-coverage";
+import "solidity-coverage";
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
